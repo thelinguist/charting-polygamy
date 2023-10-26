3835d1d993f9aa7f15f0fa999fa7a60ab288e130
Bryce S <brycebox@gmail.com>
2023-10-25 23:19:38 -0600

    feat: parley p pratt & data

--- packages/crawlers/src/main.ts
+++ packages/crawlers/src/main.ts
@@ -4,12 +4,14 @@ import { Factoid } from "./types"
 
 const famousPolygamists = {
     BrighamYoung: "Young-93",
+    ParleyPPratt: "Pratt-1938",
+    HenryMower: "Mower-364",
 }
 
 const factsFile = "facts.csv"
 
 export const main = async () => {
-    const data = await wikiTree.getPatriarchAndWives(famousPolygamists["BrighamYoung"], {})
+    const data = await wikiTree.getPatriarchAndWives(famousPolygamists["ParleyPPratt"], {})
     const facts = Object.values(data).flat() as Factoid[]
     await saveToCSV(facts, factsFile)
 }
