6cbef9b90c6ca058e007d6e8d92b9e72d22a2268
Bryce S <brycebox@gmail.com>
2023-12-31 14:39:50 -0700

    fix: render patriarchs with bad marriage data

--- packages/lib/src/util/mermaid/index.test.ts
+++ packages/lib/src/util/mermaid/index.test.ts
@@ -29,6 +29,32 @@ const wives: Timeline[] = [
         otherMarriages: [],
     },
 ]
+const polygamist = {
+    ...rootTimeline,
+    marriages: [
+        ...rootTimeline.marriages,
+        {
+            start: new Date("1890-01-01"),
+            gap: 1,
+            age: 20,
+        },
+    ],
+}
+const polygamistWives: Timeline[] = [
+    ...wives,
+    {
+        name: "spouse2",
+        birth: addYears(wives[0].birth, 1),
+        death: new Date("1931-01-01"),
+        age: 17,
+        gap: -2,
+        linkedMarriage: {
+            start: polygamist.marriages[1].start!,
+            end: polygamist.death,
+        },
+        otherMarriages: [],
+    },
+]
 describe("createChart", () => {
     it("should create a chart for traditional marriage", () => {
         const chart = createChart({ rootTimeline, wives })
@@ -52,33 +78,6 @@ describe("createChart", () => {
     })
 
     it("should create a chart for polygamist family", () => {
-        const polygamist = {
-            ...rootTimeline,
-            marriages: [
-                ...rootTimeline.marriages,
-                {
-                    start: new Date("1890-01-01"),
-                    gap: 1,
-                    age: 20,
-                },
-            ],
-        }
-        const polygamistWives: Timeline[] = [
-            ...wives,
-            {
-                name: "spouse2",
-                birth: addYears(wives[0].birth, 1),
-                death: new Date("1931-01-01"),
-                age: 17,
-                gap: -2,
-                linkedMarriage: {
-                    start: polygamist.marriages[1].start!,
-                    end: polygamist.death,
-                },
-                otherMarriages: [],
-            },
-        ]
-
         const chart = createChart({ rootTimeline: polygamist, wives: polygamistWives })
 
         // wrong indentation is expected and has to do with inline snapshot
@@ -107,43 +106,13 @@ describe("createChart", () => {
         `)
     })
 
-    it('handles missing marriage info', () => {
-        const values = `
-        gantt
-\ttitle Allen Joseph Stout and his wives
-\ttodayMarker off
-\tdateFormat YYYY-MM-DD
-\taxisFormat %Y
-    
-\tsection Allen Joseph Stout
-\tlife: 1815-12-05, 1889-12-18
-\tage -30 | 50 years younger: milestone,done, 1785-01-01, 1d
-\tage -18 | 38 years younger: milestone,done, 1797-01-01, 1d
-\tage 27 | 7 years older: milestone,done, 1843-07-17, 1d
-\tage 32 | 16 years older: milestone,done, 1848-04-30, 1d
-\t
-\tsection Margaret Stout
-\tlife: 1765-09-11, 1828-01-01
-\tmarriage: crit,1785-01-01, 1828-01-01
-\tage 19 | 50 years older: milestone,done, 1785-01-01, 1d
-\t
-\tsection Rachael Stout
-\tlife: 1777-06-23, 1827-06-26
-\tmarriage: crit,1797-01-01, 1827-06-26
-\tage 19 | 38 years older: milestone,done, 1797-01-01, 1d
-\t
-\tsection Elizabeth Anderson
-\tlife: 1823-10-13, 1848-01-30
-\tmarriage: crit,1843-07-17, 1848-01-30
-\tage 19 | 7 years younger: milestone,done, 1843-07-17, 1d
-\t
-\tsection Amanda Melvina Fisk
-\tlife: 1832-06-12, 1888-09-21
-\tmarriage: crit,1848-04-30, 1888-09-21
-\tage 15 | 16 years younger: milestone,done, 1848-04-30, 1d
-\t
-\tsection Rebecca Smith
-\tlife: 1785-06-09, 1835-09-01
-\tmarriage: `
+    it("handles missing marriage info", () => {
+        const wives = [...polygamistWives]
+        // @ts-ignore
+        wives[1].linkedMarriage.start = undefined
+        const chart = createChart({ rootTimeline: polygamist, wives })
+        const [_, spouse2] = chart.split("section spouse2")
+        expect(chart).toContain("title root and his wives")
+        expect(spouse2).not.toContain("marriage:")
     })
 })
