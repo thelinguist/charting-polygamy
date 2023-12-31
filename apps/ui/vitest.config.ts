8a350a3f9c87a243a6cb10dc6a44a1364358abdf
Bryce S <brycebox@gmail.com>
2023-12-31 15:05:58 -0700

    fix: vitest update

--- apps/ui/vitest.config.ts
+++ apps/ui/vitest.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [],
     test: {
         environment: "jsdom",
     },
