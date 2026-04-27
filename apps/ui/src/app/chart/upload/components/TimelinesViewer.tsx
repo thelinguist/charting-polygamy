995f2e6b3bb3b2cdeeaa6c2973c58d63faaba2ea
Bryce S <brycebox@gmail.com>
2026-04-26 21:05:31 -0600

    feat: contact info

--- apps/ui/src/app/chart/upload/components/TimelinesViewer.tsx
+++ apps/ui/src/app/chart/upload/components/TimelinesViewer.tsx
@@ -11,6 +11,9 @@ import { UploadButton } from "../../../../components/UploadButton"
 import { Timelines } from "./Timelines"
 import { ManualEntryForm } from "./ManualEntryForm"
 
+const contactHref = `mailto:${"chartingpolygamy"}@${"noctiluma.com"}`
+
+
 const getFileFormat = (file: File): FileTypes => {
     const ext = file.name.split(".").at(-1)
     if (ext === "ged") return FileTypes.ged
@@ -49,7 +52,9 @@ export const TimelinesViewer = () => {
             <div className={styles.chart}>
                 <p>In order to render a chart you will need either:</p>
                 <ul>
-                    <li>a gedcom file (which you can export from ancestry.com)</li>
+                    <li>
+                        a gedcom file (.ged) which you can export from ancestry.com {">"} my tree {">"} tree settings export
+                    </li>
                     <li>
                         a csv file, formatted{" "}
                         <Link href="/data/parley-p-pratt.csv" target="_blank" rel="noopener noreferrer">
@@ -58,6 +63,11 @@ export const TimelinesViewer = () => {
                     </li>
                     <li>enter family data manually using the form below</li>
                 </ul>
+                <br />
+                <p>
+                    I am collecting interesting charts for a new page. If you want to contribute, or have any feedback,{" "}
+                    <a href={contactHref}>emails are welcome</a>.
+                </p>
             </div>
             <div className={classNames(styles.chart, styles.uploadInfo)}>
                 <UploadButton
